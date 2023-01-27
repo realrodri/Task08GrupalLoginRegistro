4eabb29ae6f258e0562af257cf2656dd54256f8b
Javier Alcantarilla Ballesteros <javalc310@alu.centroafuera.es>
2023-01-27 14:19:41 +0100

    registro mejorado

--- app/src/main/java/com/example/task08grupalloginregistro/Login.kt
+++ app/src/main/java/com/example/task08grupalloginregistro/Login.kt
@@ -2,10 +2,16 @@ package com.example.task08grupalloginregistro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task08grupalloginregistro.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    var users = HashMap<String, String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
