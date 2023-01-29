49ffee76b1e3c642177b3a78f8351382995146f5
Javier Alcantarilla Ballesteros <113511967+javialc310@users.noreply.github.com>
2023-01-29 14:34:27 +0100

    Programacion donete

--- app/src/main/java/com/example/task08grupalloginregistro/Home.kt
+++ app/src/main/java/com/example/task08grupalloginregistro/Home.kt
@@ -2,10 +2,15 @@ package com.example.task08grupalloginregistro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task08grupalloginregistro.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityHomeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+
+        binding.textView.text= "Bienvenid@ ${intent.getStringExtra("USUARIO")}"
     }
 }
--- app/src/main/java/com/example/task08grupalloginregistro/Login.kt
+++ app/src/main/java/com/example/task08grupalloginregistro/Login.kt
@@ -1,7 +1,10 @@
 package com.example.task08grupalloginregistro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.task08grupalloginregistro.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
@@ -12,6 +15,71 @@ class Login : AppCompatActivity() {
         binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        botonLogin()
+        botonRegistro()
 
+    }
+
+    fun botonLogin(){
+        binding.login.setImageResource(R.drawable.login)
+        binding.login.setOnClickListener {
+            obtenerUser()
+            if (users.isNotEmpty()){
+                checkUser()
+            }else{
+                alertaUsuario()
+            }
+        }
+    }
+
+    fun botonRegistro(){
+        binding.buttonRegistro.setImageResource(R.drawable.registro)
+        binding.buttonRegistro.setOnClickListener {
+            val intent= Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun checkUser(){
+        if (users.containsKey(binding.editTextNombre.text.toString())){
+            if (users.get(binding.editTextNombre.text.toString()).equals(binding.editTextTextPassword.text.toString())){
+                val intent= Intent(this, Home::class.java)
+                intent.putExtra("USUARIO", binding.editTextNombre.text.toString())
+                startActivity(intent)
+            }else
+                alertaPass()
+        }else
+            alertaUsuario()
+    }
+
+    fun alertaUsuario(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("El usuario introducido no existe.")
+        builder.setPositiveButton("Aceptar") { dialog, which ->
+            Toast.makeText(applicationContext,
+                "Aceptar", Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
+    }
+
+    fun obtenerUser(){
+        val user= intent.getStringExtra("USER")
+        val pass= intent.getStringExtra("PASS")
+
+        if (user!=null && pass!=null)
+            users.put(user, pass)
+
+    }
+
+    fun alertaPass(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("La contraseña no es correcta.")
+        builder.setPositiveButton("Aceptar") { dialog, which ->
+            Toast.makeText(applicationContext,
+                "Aceptar", Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
     }
 }
